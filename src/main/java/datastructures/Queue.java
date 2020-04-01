64bc8420f7807f1dc16904fe9cec0da9fee440e6
Christian Preß <Christian.Press@syracos.com>
2020-04-01 02:25:58 +0200

    implemented queue

--- src/main/java/datastructures/Queue.java
+++ src/main/java/datastructures/Queue.java
@@ -1,5 +1,7 @@
 package datastructures;
 
+import java.util.NoSuchElementException;
+
 public class Queue {
 
     private class Node {
@@ -25,31 +27,27 @@ public class Queue {
         // Create a new node
         // Set the current tail.next to point to this new node
         // Then set the new node to be the new tail
-
-        Node newNode = new Node(data);
-        if (tail != null) {
-            tail.next = newNode;
-        }
-        tail = newNode;
+        Node node = new Node(data);
+        if (tail != null)
+            tail.next = node;
+        tail = node;
 
         // handle case of first element where head is null
-        if (head == null) {
-            head = tail;
-        }
+        if (head == null)
+            head = node;
     }
 
     public int remove() {
         // Save the data
         // Point the head to the next element (effectively removing it)
         // Then return the data
-
+        if (head == null) throw new NoSuchElementException("Queue empty");
         int data = head.data;
         head = head.next;
 
         // Handle queue now being empty
-        if (head == null) {
+        if (head == null)
             tail = null;
-        }
 
         return data;
     }
